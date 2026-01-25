665cba04a7de2d1670cb357a74a8e6393b616c59
rcfyl123-a11y <rcfyl123@gmail.com>
2026-01-25 23:51:02 +0300

    Add blog news with images and admin

---
+++ osiris/apps/blog/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import News
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ["title", "summary", "body", "image", "is_published"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "summary": forms.Textarea(
+                attrs={"class": "form-control", "rows": 3}
+            ),
+            "body": forms.Textarea(attrs={"class": "form-control", "rows": 8}),
+            "is_published": forms.CheckboxInput(attrs={"class": "form-check-input"}),
+        }
--- osiris/apps/blog/views.py
+++ osiris/apps/blog/views.py
@@ -1,8 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import NewsForm
+from .models import News
 
 
 NAVBAR_MESSAGE = "Welcome to the Blog App!"
 
 
-def index(request):
+def home(request):
     return render(request, "blog/index.html", {"navbar_message": NAVBAR_MESSAGE})
+
+
+def index(request):
+    news_items = News.objects.filter(is_published=True)
+    return render(
+        request,
+        "blog/news_list.html",
+        {"news_items": news_items, "navbar_message": NAVBAR_MESSAGE},
+    )
+
+
+def detail(request, pk):
+    news_item = get_object_or_404(News, pk=pk, is_published=True)
+    return render(
+        request,
+        "blog/news_detail.html",
+        {"news_item": news_item, "navbar_message": NAVBAR_MESSAGE},
+    )
+
+
+def create_news(request):
+    if request.method == "POST":
+        form = NewsForm(request.POST, request.FILES)
+        if form.is_valid():
+            news_item = form.save()
+            return redirect("blog:detail", pk=news_item.pk)
+    else:
+        form = NewsForm()
+
+    return render(
+        request,
+        "blog/news_form.html",
+        {"form": form, "form_title": "Create news", "navbar_message": NAVBAR_MESSAGE},
+    )
+
+
+def edit_news(request, pk):
+    news_item = get_object_or_404(News, pk=pk)
+
+    if request.method == "POST":
+        form = NewsForm(request.POST, request.FILES, instance=news_item)
+        if form.is_valid():
+            news_item = form.save()
+            return redirect("blog:detail", pk=news_item.pk)
+    else:
+        form = NewsForm(instance=news_item)
+
+    return render(
+        request,
+        "blog/news_form.html",
+        {"form": form, "form_title": "Edit news", "navbar_message": NAVBAR_MESSAGE},
+    )
